2d8fb4518a092b7751078907eef013a870d66b90
Ventura Lucena Martínez <ventura_lucena_99@hotmail.com>
2021-06-30 12:42:46 +0200

    Exercise 4 finished.

--- Assignment_2/exercise_4.py
+++ Assignment_2/exercise_4.py
@@ -2,31 +2,56 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.io import arff
 from sklearn.decomposition import PCA
+import sys
 
 
 plt.style.use("fivethirtyeight")
 
 
-def main():
-    data = arff.loadarff(".\iris\iris.arff")
+def loadFile(file_name):
+    # Loads the data using dataframes.
+    data = arff.loadarff(file_name)
     df = pd.DataFrame(data[0])
-    df = df[['sepallength', 'sepalwidth', 'petallength', 'petalwidth']]
-    print(df)
-    print("Iris:\n", df.describe(), "\n")
 
-    df.hist()
-    plt.show()
+    return df
 
-    pca = PCA(n_components=2)
-    principalComponents = pca.fit_transform(df)
-    principalDf = pd.DataFrame(data=principalComponents, columns=[
-        'Principal component 1', 'Principal component 2'])
 
-    print(principalDf)
-    print("Iris:\n", principalDf.describe(), "\n")
+def main():
+    if len(sys.argv) >= 2:
+        for i in range(1, len(sys.argv)):
+            file_name = sys.argv[i]
+            df = loadFile(file_name)
 
-    principalDf.hist()
-    plt.show()
+            if file_name == ".\weather\weather.arff":
+                df = df[['temperature', 'humidity']]
+
+            if file_name == ".\iris\iris.arff":
+                df = df[['sepallength', 'sepalwidth',
+                         'petallength', 'petalwidth']]
+
+            if file_name == ".\glass\glass.arff":
+                df = df[['RI', 'Na', 'Mg', 'Al', 'Si', "'K'", 'Ca', 'Ba', 'Fe']]
+
+            print(df)
+            print("\n", df.describe(), "\n")
+
+            df.hist()
+            plt.show()
+
+            pca = PCA(n_components=2)
+            principalComponents = pca.fit_transform(df)
+            principalDf = pd.DataFrame(data=principalComponents, columns=[
+                                       'P1', 'P2'])
+
+            print(principalDf)
+            print("\n", principalDf.describe(), "\n")
+            principalDf.hist()
+            plt.show()
+
+    else:
+        # Execution example:
+        #   python3 .\exercise_4.py .\weather\weather.arff .\glass\glass.arff .\iris\iris.arff
+        print("Data was not selected!")
 
 
 if __name__ == "__main__":

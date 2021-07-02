484e4171e96ea3691af05e1379703f099821ff5d
Ventura Lucena Martínez <ventura_lucena_99@hotmail.com>
2021-07-02 17:25:24 +0200

    Added exercise 4 to exercise 3.

--- Assignment_4/exercise_3_4.py
+++ Assignment_4/exercise_3_4.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.cluster.hierarchy as sch
+from sklearn import metrics
 
 from scipy.io import arff
 
@@ -21,12 +22,15 @@ def main():
     attributes = df.iloc[:, [0, 1, 2, 3, 4, 5, 6, 7, 8]].values
 
     # Method: single, complete, average.
-    h_clustering = sch.linkage(attributes, method="complete")
+    h_clustering = sch.linkage(attributes, method="single")
     dendogram = sch.dendrogram(h_clustering)
 
     # El parámetro t indica dónde corto en el dendograma, en el eje de la y.
     clusters = sch.fcluster(h_clustering, t=5, criterion="distance")
     n_clusters = np.amax(np.unique(clusters))
+
+    print("Silhouette Coefficient: %0.3f"
+          % metrics.silhouette_score(df, clusters, metric='euclidean'))
 
     df["Hierarchical Clustering"] = clusters
     # print(df)
@@ -35,7 +39,7 @@ def main():
     df.plot.scatter(x="RI", y="Si", c=clusters,
                     cmap="Greens", edgecolor="black")
 
-    plt.show()
+    # plt.show()
 
 
 if __name__ == "__main__":
